87a314343b7671f938cf885a2e700db7b7c64363
EmemBlessing <50305652+aemem10@users.noreply.github.com>
2022-06-14 18:30:55 -0500

    This is to create a test file in /var/lib (#3)
    Co-authored-by: Emem <ememb.@Emems-MacBook-Pro.local>

--- vars/team6A3App.groovy
+++ vars/team6A3App.groovy
@@ -2,5 +2,12 @@ def kaodichifunction(){
   echo "Collect recent changes to log files"
   sh 'cd /var/lib/logs'
   sh 'ls'
-  
+
  }
+ def Blessingfunction(){
+  echo "This is to create filename"
+  sh 'cat This is a test file for project 10 > team6.txt'
+  sh 'cat team6.txt'
+
+ }
+
