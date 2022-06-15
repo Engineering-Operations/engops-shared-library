ccc6ee9ea4207a5990fdba08aca4da6fb1f4b0da
EmemBlessing <50305652+aemem10@users.noreply.github.com>
2022-06-14 22:19:59 -0500

    Update team6A3App.groovy

--- vars/team6A3App.groovy
+++ vars/team6A3App.groovy
@@ -1,14 +1,25 @@
 def kaodichifunction(String repoUrl){
-  echo "Collect recent changes to log files"
-  sh 'cd /var/lib/logs'
-  sh 'ls'
- }
+pipeline{
+    agent any
+    stages{
+        stage('parallel-job'){
+            parallel{
+                stage('Collect Recent'){
+                    echo "Collect recent changes to log files"
+                    sh 'cd /var/lib/logs'
+                    sh 'ls'
+                }
+            }
+            stage('code-deploy'){
+                steps {
+                    git branch: 'main',
+                    url: "${repoUrl}"
+                }
+            }
+        }
 
-stage('code-deploy'){
-  steps {
-     git branch: 'main',
-       url: "${repoUrl}"
-  }
+    }
 }
+
 
  
