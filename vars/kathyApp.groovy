37a01af86bcc1d58e682716e04f18fbf82e44321
EtechEng <106220256+EtechEng@users.noreply.github.com>
2022-06-15 22:20:11 -0400

    Create kathyApp.groovy

---
+++ vars/kathyApp.groovy
@@ -0,0 +1,34 @@
+
+def call(String repoUrl){
+  pipeline{
+    agent any
+    stages{
+        stage('version-control'){
+            steps{
+                checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'github-id', url: 'https://github.com/Engineering-Operations/parallel-multib-pipeline.git']]])
+            }
+        }
+        stage('parallel-job'){
+            parallel{
+                stage('Stage 1'){
+                    steps{
+                      echo 'This is to check disk space'
+                      sh 'df -h'
+                    }
+                }
+                stage('Stage 2'){
+                    steps{
+                      echo 'This is to make a new directory'
+                      sh 'mkdir kathy'
+                    }
+                }
+            }
+        }
+        stage('code-deploy'){
+            steps {
+                git branch: 'main',
+                url: "${repoUrl}"
+            }
+        }
+    }
+}
