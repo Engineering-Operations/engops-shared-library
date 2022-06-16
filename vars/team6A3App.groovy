e39870c164f1da31fc47556c7dafa993f466088f
EmemBlessing <50305652+aemem10@users.noreply.github.com>
2022-06-16 09:44:15 -0500

    Update team6A3App.groovy

--- vars/team6A3App.groovy
+++ vars/team6A3App.groovy
@@ -1,20 +1,56 @@
-def kaodichifunction(String repoUrl){
-pipeline{
+def call(String repoUrl){
+  pipeline{
     agent any
     stages{
+        stage('version-control'){
+            steps{
+                checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: 'github-id', url: 'https://github.com/Engineering-Operations/parallel-multib-pipeline.git']]])
+            }
+        }
         stage('parallel-job'){
             parallel{
-                stage('Collect Recent'){
-                    echo "Collect recent changes to log files"
-                    sh 'cd /var/lib/logs'
-                    sh 'ls'
+                stage('syscheck'){
+                    steps{
+                      echo 'This is to check for operating system statistics'
+                      sh 'cat /etc/os-release'
+                    }
                 }
-            }
-            stage('code-deploy'){
-                steps {
-                    git branch: 'main',
-                    url: "${repoUrl}"
+                stage('jenkins-stat'){
+                    steps{
+                      echo 'This is to check the current status of jenkins'
+                      sh 'sudo systemctl status jenkins'
+                    }
                 }
             }
         }
+        stage('parallel-job2'){
+            parallel{
+               stage('disk-free'){
+                    steps{
+                      echo 'This is to check disk free space'
+                      sh 'df -h'
+                    }
+               }
+                stage('disk-usage'){
+                    steps{
+                      echo 'this is to check disk usage'
+                      sh 'du -h'
+                    }
+               }
+                stage('check memory free space in megabytes'){
+                    steps{
+                      echo 'this is to check memory free space megabytes'
+                      sh 'free -m'
+                    }
+                }
+            }   
+        }
+        stage('code-deploy'){
+            steps {
+                git branch: 'main',
+                url: "${repoUrl}"
+            }
+        }
     }
+}
+}
